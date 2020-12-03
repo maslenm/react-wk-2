64b6360f9966d3a445766b321d645cc4f2fe2118
Matt Maslen <mattmaslen123@gmail.com>
2020-12-03 20:14:52 +0000

    updated to use condition

--- src/components/hooks/Clicked.js
+++ src/components/hooks/Clicked.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 const Clicked = () => {
-	const [click, setClick] = useState("Not clicked!");
+	const [click, setClick] = useState(false);
 
-	const updateClick = () => setClick("Clicked!");
+	const updateClick = () => setClick(true);
 
-	return <p onClick={updateClick}>{click}</p>;
+	return <p onClick={updateClick}>{click ? "Clicked!" : "Not clicked!"}</p>;
 };
 
 export default Clicked;

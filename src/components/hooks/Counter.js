9e845947ae54e60e91279b7ffb331f27721791ad
Matt Maslen <mattmaslen123@gmail.com>
2020-12-03 20:39:58 +0000

    updated where condition is used following review

--- src/components/hooks/Counter.js
+++ src/components/hooks/Counter.js
@@ -3,14 +3,14 @@ import { useState } from "react";
 const Counter = ({ initial, max }) => {
 	const [counter, setCounter] = useState(initial);
 
-	const updateCounterPlus = () => setCounter(counter + 1);
+	const updateCounterPlus = () => setCounter(counter + (counter < max ? 1 : 0));
 
-	const updateCounterMinus = () => setCounter(counter - 1);
+	const updateCounterMinus = () => setCounter(counter - (counter > 0 ? 1 : 0));
 	return (
 		<>
-			<button onClick={counter < max ? updateCounterPlus : null}>+</button>
+			<button onClick={updateCounterPlus}>+</button>
 			<p>{counter}</p>
-			<button onClick={counter > 0 ? updateCounterMinus : null}>-</button>
+			<button onClick={updateCounterMinus}>-</button>
 		</>
 	);
 };

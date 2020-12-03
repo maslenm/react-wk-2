22291279ad931b776d983fd5f06c4f39d2498095
Matt Maslen <mattmaslen123@gmail.com>
2020-12-03 20:29:31 +0000

    updated where condition is used following review

--- src/components/hooks/StepCounter.js
+++ src/components/hooks/StepCounter.js
@@ -3,15 +3,15 @@ import { useState } from "react";
 const StepCounter = ({ max, step }) => {
 	const [count, setCount] = useState(0);
 
-	const updateCountPlus = () => setCount(count + step);
+	const updateCountPlus = () => setCount(count + (count < max ? step : 0));
 
-	const updateCountMinus = () => setCount(count - step);
+	const updateCountMinus = () => setCount(count - (count > 0 ? step : 0));
 
 	return (
 		<>
-			<button onClick={count < max ? updateCountPlus : null}>+</button>
+			<button onClick={updateCountPlus}>+</button>
 			<p>{count}</p>
-			<button onClick={count > 0 ? updateCountMinus : null}>-</button>
+			<button onClick={updateCountMinus}>-</button>
 		</>
 	);
 };
